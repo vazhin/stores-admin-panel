de1b5fdd059d38cefd1c80a06383f6ed61f6bd8f
Vazhin Tayeb <vazhintayeb@gmail.com>
2020-12-11 02:48:00 +0300

    creates a new product in post route

--- api/controllers/products.js
+++ api/controllers/products.js
@@ -2,10 +2,17 @@ const { Product } = require('../models');
 const dbService = require('../services/db');
 
 exports.createProduct = async (req, res, next) => {
-  const { name, image, price, quantity, categoryId } = req.body;
+  const { name, price, quantity, categoryId } = req.body;
+  const image = req.file ? req.file.path : '';
   try {
-    //
-    res.status(200).json();
+    const product = await dbService.create(Product, {
+      name,
+      image,
+      price: parseInt(price),
+      quantity: parseInt(quantity),
+      categoryId: parseInt(categoryId),
+    });
+    res.status(200).json(product);
   } catch (err) {
     console.log(err);
     res.status(500).json(err);
