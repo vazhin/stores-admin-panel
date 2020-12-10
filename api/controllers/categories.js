d5dff5c03ba288f000d176d6d61efdc72df83113
Vazhin Tayeb <vazhintayeb@gmail.com>
2020-12-11 02:56:51 +0300

    creates a new category in post route

--- api/controllers/categories.js
+++ api/controllers/categories.js
@@ -1,11 +1,16 @@
 const { Category, Product } = require('../models');
-const dbService = require('../services/db');
+const db = require('../services/db');
 
 exports.createCategory = async (req, res, next) => {
-  const { name, image, storeId } = req.body;
+  const { name, storeId } = req.body;
+  const image = req.file ? req.file.path : '';
   try {
-    //
-    res.status(200).json();
+    const category = await db.create(Category, {
+      name,
+      image,
+      storeId: parseInt(storeId),
+    });
+    res.status(200).json(category);
   } catch (err) {
     console.log(err);
     res.status(500).json(err);
@@ -16,7 +21,7 @@ exports.getProductsInCategory = async (req, res, next) => {
   const pageNum = req.query.page ? req.query.page : 1;
   const categoryId = req.params.categoryId;
   try {
-    const products = await dbService.getAllById(
+    const products = await db.getAllById(
       Product,
       Category,
       { field: 'categoryId', value: categoryId },
