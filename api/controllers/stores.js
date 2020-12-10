78a867df6f39194bec13d5d54c67930f8131dde8
Vazhin Tayeb <vazhintayeb@gmail.com>
2020-12-10 19:58:14 +0300

    gets all the stores

--- api/controllers/stores.js
+++ api/controllers/stores.js
@@ -1,3 +1,22 @@
+const { Store } = require('../models');
+const { Op } = require('sequelize');
+
 exports.getStores = async (req, res, next) => {
-  res.send('Stores');
+  const pageNum = req.query.page ? req.query.page : 1;
+  const limit = 8;
+  try {
+    const result = await Store.findAndCountAll({
+      offset: (pageNum - 1) * limit,
+      limit,
+      order: [['createdAt', 'DESC']],
+    });
+
+    const stores = result.rows;
+    const numOfStores = result.count;
+    const numOfPages = Math.ceil(numOfStores / limit);
+
+    res.status(200).json({ stores, numOfStores, numOfPages });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 };
