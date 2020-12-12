603138f69396e71a1758ba070b6d0b41aec187c0
Vazhin Tayeb <vazhintayeb@gmail.com>
2020-12-12 19:34:00 +0300

    creates store on form submit

--- client/src/components/form.js
+++ client/src/components/form.js
@@ -1,71 +1,97 @@
 import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
 import { Form, Button } from 'react-bootstrap';
+import db from '../services/dataService';
 
 const CreateItemForm = ({ table }) => {
+  const history = useHistory();
   const [validated, setValidated] = useState(false);
+  const formData = new FormData();
 
-  const handleSubmit = (event) => {
+  const handleSubmit = async (event) => {
     event.preventDefault();
     const form = event.currentTarget;
     if (form.checkValidity() === false) {
       event.stopPropagation();
+    } else {
+      await db.create(formData, table);
+      history.go(0);
     }
 
     setValidated(true);
   };
 
-  const fields = [];
-
   switch (table) {
     case 'stores':
-      fields.push('name', 'logo');
+      formData.append('name', '');
+      formData.append('logo', '');
       break;
     case 'categories':
-      fields.push('name', 'image');
+      formData.append('name', '');
+      formData.append('image', '');
+
       break;
     case 'products':
-      fields.push('name', 'price', 'quantity', 'image');
+      formData.append('name', '');
+      formData.append('price', '');
+      formData.append('quantity', 0);
+      formData.append('image', '');
       break;
     default:
       break;
   }
+  const fields = [];
+
+  for (var key of formData.keys()) {
+    fields.push(key);
+  }
 
   return (
-    <Form noValidate validated={validated} onSubmit={handleSubmit}>
-      {fields.map((field, i) =>
-        ['logo', 'image'].includes(field) ? (
-          <Form.Group>
-            <Form.Label>
-              {field.charAt(0).toUpperCase() + field.slice(1)}
-            </Form.Label>
-            <Form.File id="formcheck-api-custom" custom>
-              <Form.File.Input required />
-              <Form.File.Label>Choose Image</Form.File.Label>
-              <Form.Control.Feedback type="invalid">
-                You must upload an image
-              </Form.Control.Feedback>
-            </Form.File>
-          </Form.Group>
-        ) : (
-          <Form.Group controlId="validationCustom01">
-            <Form.Label>
-              {field.charAt(0).toUpperCase() + field.slice(1)}
-            </Form.Label>
-            <Form.Control
-              required
-              type="text"
-              placeholder={`${field.charAt(0).toUpperCase() + field.slice(1)}`}
-              // defaultValue="Mark"
-            />
-            <Form.Control.Feedback type="invalid">
-              The {field} must not be empty
-            </Form.Control.Feedback>
-          </Form.Group>
-        )
-      )}
+    <>
+      {fields.length !== 0 && (
+        <Form noValidate validated={validated} onSubmit={handleSubmit}>
+          {fields.map((field, i) =>
+            ['logo', 'image'].includes(field) ? (
+              <Form.Group key={i}>
+                <Form.Label>
+                  {field.charAt(0).toUpperCase() + field.slice(1)}
+                </Form.Label>
+                <Form.File id="formcheck-api-custom" custom>
+                  <Form.File.Input
+                    required
+                    onChange={(e) => formData.set(field, e.target.files[0])}
+                  />
+                  <Form.File.Label>Choose Image</Form.File.Label>
+                  <Form.Control.Feedback type="invalid">
+                    You must upload an image
+                  </Form.Control.Feedback>
+                </Form.File>
+              </Form.Group>
+            ) : (
+              <Form.Group key={i} controlId="validationCustom01">
+                <Form.Label>
+                  {field.charAt(0).toUpperCase() + field.slice(1)}
+                </Form.Label>
+                <Form.Control
+                  required
+                  type="text"
+                  placeholder={`${
+                    field.charAt(0).toUpperCase() + field.slice(1)
+                  }`}
+                  onChange={(e) => formData.set(field, e.target.value)}
+                  // defaultValue="Mark"
+                />
+                <Form.Control.Feedback type="invalid">
+                  The {field} must not be empty
+                </Form.Control.Feedback>
+              </Form.Group>
+            )
+          )}
 
-      <Button type="submit">Add Item</Button>
-    </Form>
+          <Button type="submit">Add Item</Button>
+        </Form>
+      )}
+    </>
   );
 };
 
